4c95c9ec4ca35fede91c15160ca28d861c5ef72f
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-11-19 10:54:18 -0300

    primeiro

---
+++ conta.py
@@ -0,0 +1,21 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("contruindo obejeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if (valor <= self.saldo):
+            self.saldo -= valor
+        else:
+            print("saldo baixo")
