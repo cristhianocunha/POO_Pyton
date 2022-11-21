f3f200c7897e0424d2eb2608e06ec68dee587764
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-11-21 18:14:04 -0300

    Aula todas do mod 06.

--- conta.py
+++ conta.py
@@ -8,17 +8,42 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def depositar(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self,valor_a_sacar ):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
     def saque(self, valor):
-        if (valor <= self.__saldo):
+        if (self.__pode_sacar(valor)):
             self.__saldo -= valor
         else:
             print("saldo baixo")
+
     def transferir(self, valor, destino):
         self.saque(valor)
         destino.depositar(valor)
+
+    @property
+    def titular(self):
+        return self.__titular
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+        print("Novo limite R$ {}".format(self.__limite))
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
